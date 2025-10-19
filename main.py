439bf33c72a30aefad74be0ab4b8da0197775e0d
Ilham <86422270+titanilham@users.noreply.github.com>
2025-10-19 21:20:06 +0300

    update lab5

--- main.py
+++ main.py
@@ -19,24 +19,25 @@ class Task1:
     @classmethod
     def rectangle(cls, a, b):
         return a * b
+class Task2:
+    @classmethod
+    def task2(cls, arr1, arr2, arr3):
+        if len(arr1) > 15 or len(arr2) > 15 or len(arr3) > 15:
+            raise ValueError("Размер каждого массива не должен превышать 15")
 
-def task2(arr1, arr2, arr3):
-    if len(arr1) > 15 or len(arr2) > 15 or len(arr3) > 15:
-        raise ValueError("Размер каждого массива не должен превышать 15")
-
-    arr1 = array("l", arr1)
-    arr2 = array("l", arr2)
-    arr3 = array("l", arr3)
-    
-    average_arr1 = sum(arr1) / len(arr1)
-    average_arr2 = sum(arr2) / len(arr2)
-    average_arr3 = sum(arr3) / len(arr3)
-    
-    return f"""sum arr1: {sum(arr1)}, average arr1: {average_arr1}
-sum arr2: {sum(arr2)}, average arr2: {average_arr2}
-sum arr3: {sum(arr3)}, average arr3: {average_arr3}"""
+        arr1 = array("l", arr1)
+        arr2 = array("l", arr2)
+        arr3 = array("l", arr3)
+        
+        average_arr1 = sum(arr1) / len(arr1)
+        average_arr2 = sum(arr2) / len(arr2)
+        average_arr3 = sum(arr3) / len(arr3)
+        
+        return f"""sum arr1: {sum(arr1)}, average arr1: {average_arr1}
+    sum arr2: {sum(arr2)}, average arr2: {average_arr2}
+    sum arr3: {sum(arr3)}, average arr3: {average_arr3}"""
 
 if __name__ == "__main__":
-    print(task2([1, 2], [3, 2], [1, 2, 3, 4]))
+    print(Task2.task2([1, 2], [3, 2], [1, 2, 3, 4]))
 
 
