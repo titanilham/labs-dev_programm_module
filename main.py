155069c324bd7a5c6d18742a041b8a6c51207348
Ilham <86422270+titanilham@users.noreply.github.com>
2025-10-19 22:55:21 +0300

    add task2 lab6

--- main.py
+++ main.py
@@ -5,12 +5,28 @@ def task1(a):
         if item[i] >= 0:
             summ += item[i]
             count += 1
-    return f"{summ}, {count}"
+    return f"{summ}, {count}" 
 
-a = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, -1]]
+b = [
+    [13, 90, 0],
+    [0, 2, 3],
+    [323, 23, -1],
+    [-1, -12, -213213, 32, -21]
+]
+
+def task2(b):
+    for i in b:
+        minimum_element = i.index(min(i))
+        first_element = i[0]
+        i[0] = min(i)
+        i[minimum_element] = first_element
+        
+        maximum_element = i.index(max(i))
+        last_element = i[-1]
+        i[-1] = max(i)
+        i[maximum_element] = last_element 
+                
+    return b
 
 if __name__ == "__main__":
-    print(task1(a))
+    print(task2(b))
