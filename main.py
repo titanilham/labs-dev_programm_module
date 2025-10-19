40b1191fb13c3bf0a609561ec4fb26bcb4370716
Ilham <86422270+titanilham@users.noreply.github.com>
2025-10-19 21:56:06 +0300

    add task1 lab6

--- main.py
+++ main.py
@@ -1,43 +1,16 @@
-from math import pi
-from array import array
+def task1(a):
+    summ = 0
+    count = 0
+    for i, item in enumerate(a):
+        if item[i] >= 0:
+            summ += item[i]
+            count += 1
+    return f"{summ}, {count}"
 
-class Task1:
-    """Класс для вычисления площади разных геометрических фигур."""
-    
-    @classmethod
-    def triangle(cls, h, b):
-        return 1/2*h*b
-    
-    @classmethod
-    def circumference(cls, r):
-        return pi * r**2
-    
-    @classmethod
-    def parallelogram(cls, b, h):
-        return b * h
-    
-    @classmethod
-    def rectangle(cls, a, b):
-        return a * b
-class Task2:
-    @classmethod
-    def task2(cls, arr1, arr2, arr3):
-        if len(arr1) > 15 or len(arr2) > 15 or len(arr3) > 15:
-            raise ValueError("Размер каждого массива не должен превышать 15")
-
-        arr1 = array("l", arr1)
-        arr2 = array("l", arr2)
-        arr3 = array("l", arr3)
-        
-        average_arr1 = sum(arr1) / len(arr1)
-        average_arr2 = sum(arr2) / len(arr2)
-        average_arr3 = sum(arr3) / len(arr3)
-        
-        return f"""sum arr1: {sum(arr1)}, average arr1: {average_arr1}
-    sum arr2: {sum(arr2)}, average arr2: {average_arr2}
-    sum arr3: {sum(arr3)}, average arr3: {average_arr3}"""
+a = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, -1]]
 
 if __name__ == "__main__":
-    print(Task2.task2([1, 2], [3, 2], [1, 2, 3, 4]))
-
-
+    print(task1(a))
